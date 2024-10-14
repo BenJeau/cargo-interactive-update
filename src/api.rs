c145c96b271053d49289a19c35b5fda6b5b686d6
Benoit Jeaurond <benoit@jeaurond.dev>
2024-10-14 15:02:31 -0400

    fix: trim string fields, use latest stable version/date, update user-agent with repo details

--- src/api.rs
+++ src/api.rs
@@ -10,35 +10,44 @@ pub struct CratesIoResponse {
     pub current_version_date: Option<String>,
 }
 
+fn get_string_from_value(
+    value: Option<&serde_json::Map<String, serde_json::Value>>,
+    key: &str,
+) -> Option<String> {
+    value
+        .and_then(|d| d.get(key))
+        .and_then(|s| s.as_str())
+        .map(|s| s.trim().to_string())
+}
+
+fn get_field_from_versions(
+    versions: Option<&Vec<serde_json::Value>>,
+    version: &str,
+    key: &str,
+) -> Option<String> {
+    versions.and_then(|v| {
+        v.iter()
+            .find(|v| v.get("num").and_then(|v| v.as_str()).unwrap_or("") == version)
+            .and_then(|v| v.get(key))
+            .and_then(|s| s.as_str())
+            .map(|s| s.trim().to_string())
+    })
+}
+
 impl CratesIoResponse {
     fn from_value(value: serde_json::Value, version: &str) -> Self {
         let data = value.get("crate").and_then(|c| c.as_object());
         let versions = value.get("versions").and_then(|c| c.as_array());
 
+        let latest_version = get_string_from_value(data, "max_stable_version")
+            .unwrap_or_else(|| version.to_string());
+
         Self {
-            repository: data
-                .and_then(|d| d.get("repository"))
-                .and_then(|s| s.as_str())
-                .map(|s| s.to_string()),
-            description: data
-                .and_then(|d| d.get("description"))
-                .and_then(|s| s.as_str())
-                .map(|s| s.to_string()),
-            latest_version: data
-                .and_then(|d| d.get("newest_version"))
-                .and_then(|s| s.as_str())
-                .map(|s| s.to_string())
-                .unwrap_or_else(|| version.to_string()),
-            latest_version_date: data
-                .and_then(|d| d.get("updated_at"))
-                .and_then(|s| s.as_str())
-                .map(|s| s.to_string()),
-            current_version_date: versions.and_then(|v| {
-                v.iter()
-                    .find(|v| v["num"].as_str().unwrap_or("") == version)
-                    .and_then(|v| v["updated_at"].as_str())
-                    .map(|s| s.to_string())
-            }),
+            repository: get_string_from_value(data, "repository"),
+            description: get_string_from_value(data, "description"),
+            latest_version_date: get_field_from_versions(versions, &latest_version, "updated_at"),
+            current_version_date: get_field_from_versions(versions, version, "updated_at"),
+            latest_version,
         }
     }
 }
@@ -47,7 +56,14 @@ pub fn get_latest_version(
     CargoDependency { name, version }: &CargoDependency,
 ) -> Result<CratesIoResponse, Box<dyn std::error::Error>> {
     let mut headers = List::new();
-    headers.append("User-Agent: cargo-interactive-update (tbd under development)")?;
+
+    let package_name = env!("CARGO_PKG_NAME");
+    let package_repository = env!("CARGO_PKG_REPOSITORY");
+
+    // As required by the crates.io API - https://doc.rust-lang.org/cargo/reference/registry-web-api.html
+    headers.append(&format!(
+        "User-Agent: {package_name} ({package_repository})"
+    ))?;
 
     let mut body = vec![];
     let mut handle = Easy::new();
@@ -84,17 +100,20 @@ mod tests {
     fn test_crates_io_response_from_value() {
         let response = serde_json::json!({
             "crate": {
-                "repository": "https://github.com/user/repo",
-                "description": "A description",
-                "max_version": "0.1.0",
-                "newest_version": "0.1.0",
-                "updated_at": "2023-07-01T00:00:00Z",
+                "repository": "\thttps://github.com/user/repo ",
+                "description": " A description\n ",
+                "max_stable_version": "0.2.0",
             },
             "versions": [
                 {
                     "num": "0.1.0",
-                    "updated_at": "2023-07-01T00:00:00Z"
-                }
+                    "updated_at": " 2023-07-01T00:00:00Z\n"
+                },
+                {
+                    "num": "0.2.0",
+                    "updated_at": "2023-07-02T00:00:00Z"
+                },
+                {}
             ]
         });
 
@@ -105,32 +124,15 @@ mod tests {
             Some("https://github.com/user/repo".to_string())
         );
         assert_eq!(response.description, Some("A description".to_string()));
-        assert_eq!(response.latest_version, "0.1.0");
+        assert_eq!(response.latest_version, "0.2.0");
         assert_eq!(
             response.latest_version_date,
-            Some("2023-07-01T00:00:00Z".to_string())
+            Some("2023-07-02T00:00:00Z".to_string())
         );
         assert_eq!(
             response.current_version_date,
             Some("2023-07-01T00:00:00Z".to_string())
         );
-    }
-
-    #[test]
-    fn test_crates_io_response_from_value_only_newest_version() {
-        let response = serde_json::json!({
-            "crate": {
-                "newest_version": "0.1.0",
-            },
-        });
-
-        let response = CratesIoResponse::from_value(response, "0.1.0");
-
-        assert_eq!(response.repository, None);
-        assert_eq!(response.description, None);
-        assert_eq!(response.latest_version, "0.1.0");
-        assert_eq!(response.latest_version_date, None);
-        assert_eq!(response.current_version_date, None);
     }
 
     #[test]
