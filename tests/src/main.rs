2c0321a3c0c0058260cb426599dc8e10f47c9446
Benoit Jeaurond <benoit@jeaurond.dev>
2025-01-17 21:44:19 -0500

    feat: make the e2e tests verify more fields

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,22 +1,51 @@
 use rexpect::spawn;
 use std::path::Path;
+use toml_edit::DocumentMut;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     able_to_quit()?;
-    update_package("single_package")?;
-    update_package("multiple_packages")?;
+
+    let single_package = update_package("single_package")?;
+    for (dep_type, pkg, version) in [
+        ("dependencies", "base64", "0.1.0"),
+        ("dev-dependencies", "unicode-ident", "1.0.0"),
+        ("build-dependencies", "rand_core", "0.1.0"),
+    ] {
+        assert_ne!(single_package[dep_type][pkg].as_str(), Some(version));
+    }
+
+    let multiple_packages = update_package("multiple_packages")?;
+    for (dep_type, pkg, version) in [
+        ("dependencies", "base64", "0.1.0"),
+        ("dev-dependencies", "unicode-ident", "1.0.0"),
+        ("build-dependencies", "rand_core", "0.1.0"),
+    ] {
+        assert_ne!(multiple_packages[dep_type][pkg].as_str(), Some(version));
+    }
+
+    verify_content_of_workspaces()?;
+
     Ok(())
 }
 
-fn update_all_deps() -> Result<(), Box<dyn std::error::Error>> {
+fn able_to_quit() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Testing ability to quit");
+
+    let original_dir = std::env::current_dir()?;
+
+    let package_path = Path::new("./packages").join("single_package");
+    std::env::set_current_dir(&package_path)?;
+
     let mut session = spawn("cargo interactive-update", Some(3000))?;
     session.exp_string(" to select/deselect, ")?;
-    session.send_line("a\r")?;
+    session.send_line("q\r")?;
     session.exp_eof()?;
+
+    std::env::set_current_dir(original_dir)?;
     Ok(())
 }
 
-fn update_package(package_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn update_package(package_name: &str) -> Result<DocumentMut, Box<dyn std::error::Error>> {
     println!("Updating package: {}", package_name);
 
     let original_dir = std::env::current_dir()?;
@@ -38,22 +67,43 @@ fn update_package(package_name: &str) -> Result<(), Box<dyn std::error::Error>> 
     assert!(cargo_lock_path.exists(), "Cargo.lock was not created");
 
     std::env::set_current_dir(original_dir)?;
+    Ok(toml_content.parse().unwrap())
+}
+
+fn update_all_deps() -> Result<(), Box<dyn std::error::Error>> {
+    let mut session = spawn("cargo interactive-update", Some(3000))?;
+    session.exp_string(" to select/deselect, ")?;
+    session.send_line("a\r")?;
+    session.exp_eof()?;
     Ok(())
 }
 
-fn able_to_quit() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Testing ability to quit");
-
-    let original_dir = std::env::current_dir()?;
-
-    let package_path = Path::new("./packages").join("single_package");
-    std::env::set_current_dir(&package_path)?;
-
-    let mut session = spawn("cargo interactive-update", Some(3000))?;
-    session.exp_string(" to select/deselect, ")?;
-    session.send_line("q\r")?;
-    session.exp_eof()?;
-
-    std::env::set_current_dir(original_dir)?;
+fn verify_content_of_workspaces() -> Result<(), Box<dyn std::error::Error>> {
+    let package_path = Path::new("./packages").join("multiple_packages");
+    let first_package = Path::new(&package_path).join("first-package");
+    let second_package = Path::new(&package_path).join("second-package");
+    let first_package_toml = std::fs::read_to_string(Path::new(&first_package).join("Cargo.toml"))?
+        .parse::<DocumentMut>()
+        .unwrap();
+    let second_package_toml =
+        std::fs::read_to_string(Path::new(&second_package).join("Cargo.toml"))?
+            .parse::<DocumentMut>()
+            .unwrap();
+    assert_eq!(
+        first_package_toml["dependencies"]["base64"]["workspace"].as_bool(),
+        Some(true)
+    );
+    assert_eq!(
+        first_package_toml["dev-dependencies"]["unicode-ident"]["workspace"].as_bool(),
+        Some(true)
+    );
+    assert_eq!(
+        second_package_toml["build-dependencies"]["base64"]["workspace"].as_bool(),
+        Some(true)
+    );
+    assert_ne!(
+        second_package_toml["build-dependencies"]["unicode-ident"].as_str(),
+        Some("1.0.0")
+    );
     Ok(())
 }
